a79dd6ef12207482e57ddb62ab0d2b9054aaa2eb
nelam <nikita.gupta16@gmail.com>
2016-05-28 15:36:23 +0800

    Update lexical_scoping.R

--- lexical_scoping.R
+++ lexical_scoping.R
@@ -1,3 +1,8 @@
+## Caching the Inverse of a Matrix:
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
+## This function creates a special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -12,6 +17,11 @@ makeCacheMatrix <- function(x = matrix()) {
              setInverse = setInverse,
              getInverse = getInverse)
 }
+
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getInverse()
